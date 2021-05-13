aa8f8610e9c2d2efd3462386d91d508131c38c75
Nick <guyinhaze@gmail.com>
2021-05-13 15:58:05 +0300

    burger menu prototype

--- src/routes/Main/components/BurgerMenu/BurgerMenu.tsx
+++ src/routes/Main/components/BurgerMenu/BurgerMenu.tsx
@@ -7,47 +7,50 @@ export default function BurgerMenu() {
     const history = useHistory()
     const [BM, setBM] = useState(false)
     const showBM = () => setBM(!BM)
-    
+
     return (
         <div className="main">
-            <button className={!BM ? 'BurgerMenuIcon active' : 'BurgerMenuIcon'} onClick={showBM}></button>
+            <button
+                className={!BM ? 'BurgerMenuIcon active' : 'BurgerMenuIcon'}
+                onClick={showBM}
+            ></button>
             <div className={BM ? 'BurgerMenu active' : 'BurgerMenu'}>
-                <div className={"SideBar"}>
-                    <div className={"User"}>
-                        <div className={"UserPic"}></div>
-                        <div className={"UserData"}>
+                <div className={'SideBar'}>
+                    <div className={'User'}>
+                        <div className={'UserPic'}></div>
+                        <div className={'UserData'}>
                             <p>UserName</p>
                             <p>mail</p>
                         </div>
                     </div>
                     <div className="PagesList">
                         <div>
-                            <Link to="/">Главная</Link>
+                            <Link className='text-link' to="/">Главная</Link>
                         </div>
                         <div>
-                            <Link to="/favouriteRecipes">Любимые рецепты</Link>
+                            <Link className='text-link' to="/favouriteRecipes">Любимые рецепты</Link>
                         </div>
                         <div>
-                            <Link to="/preferences">Предпочтения</Link>
+                            <Link className='text-link'to="/preferences">Предпочтения</Link>
                         </div>
                         <div>
-                            <Link to="/requestHistory">История запросов</Link>
+                            <Link className='text-link' to="/requestHistory">История запросов</Link>
                         </div>
-                        <div></div>
+                        <div id="space"></div>
                         <div>
-                            <Link to="/settings">Настройки</Link>
+                            <Link className='text-link'to="/settings">Настройки</Link>
                         </div>
                         <div>
-                            <Link to="/support">Служба поддержки</Link>
+                            <Link className='text-link' to="/support">Служба поддержки</Link>
                         </div>
                     </div>
-                    <div className={"FeedBack"}>
+                    <div className={'FeedBack'}>
                         <p>
                             Нравится GudFud? Оставьте отзыв, чтобы мы могли
                             становиться еще лучше!
                         </p>
                         <a
-                            className={"FeedBackButton"}
+                            className={'FeedBackButton'}
                             onClick={() => {
                                 history.push('/FeedBack')
                             }}
@@ -56,7 +59,7 @@ export default function BurgerMenu() {
                         </a>
                     </div>
                 </div>
-                <div className={"DeadZone"} onClick={showBM}></div>
+                <div className={'DeadZone'} onClick={showBM}></div>
             </div>
         </div>
     )
