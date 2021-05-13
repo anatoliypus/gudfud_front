12060d1f5c510d47c3e4db15258a18930d35b849
Anatoliy <anatoliypuss5@gmail.com>
2021-05-13 23:12:39 +0300

    added component to the tree

--- src/routes/Main/Main.tsx
+++ src/routes/Main/Main.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import BurgerMenu from './components/BurgerMenu/BurgerMenu'
 import { Header } from './components/Header/Header'
 import RecipesFeed from './components/RecipesFeed/RecipesFeed'
 import WelcomeSearch from './components/WelcomeSearch/WelcomeSearch'
@@ -6,6 +7,7 @@ import WelcomeSearch from './components/WelcomeSearch/WelcomeSearch'
 export function Main() {
     return (
         <div>
+            <BurgerMenu />
             <Header />
             <WelcomeSearch />
             <RecipesFeed />
